747727f5c3b2188cbfa98947b6ca0b14f2cb5889
RustamSaga <rustam.saga94@gmail.com>
2023-06-04 17:38:34 +0300

    ViewModel test

---
+++ app/src/test/java/com/rustamsaga/numbers/main/numbers/presentation/NumbersResultMapperTest.kt
@@ -0,0 +1,40 @@
+package com.rustamsaga.numbers.main.numbers.presentation
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NumbersResultMapperTest : BaseTest() {
+
+    @Test
+    fun test_error() {
+        val communications = TestNumbersCommunications()
+        val mapper = NumbersResultMapper(communications, NumberUiMapper())
+
+        mapper.map("not empty message")
+
+        assertEquals(UiState.ShowError("not empty message"), communications.stateCalledList[0])
+    }
+
+    @Test
+    fun test_success_no_list() {
+        val communications = TestNumbersCommunications()
+        val mapper = NumbersResultMapper(communications, NumberUiMapper())
+
+        mapper.map(emptyList())
+
+        assertEquals(0, communications.timesShowList)
+        assertEquals(true, communications.stateCalledList[0] is UiState.Success)
+    }
+
+    @Test
+    fun test_success_with_list() {
+        val communications = TestNumbersCommunications()
+        val mapper = NumbersResultMapper(communications, NumberUiMapper())
+
+        mapper.map(listOf(NumberFact("5", "fact 5")))
+
+        assertEquals(true, communications.stateCalledList[0] is UiState.Success)
+        assertEquals(1, communications.timesShowList)
+        assertEquals(NumberUi("5", "fact 5"), communications.numbersList[0])
+    }
+}
